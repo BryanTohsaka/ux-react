74577bde9e83a5d0ff7d3707b19095992719b91f
Bryan <bryan@Air-de-Marisol.localdomain>
2024-05-27 14:54:41 -0600

    change icons from navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,8 @@
 import { Link } from "react-router-dom";
+import {Home} from "lucide-react";
+import {User} from "lucide-react";
+import {BriefcaseBusiness} from "lucide-react";
+import {Phone} from "lucide-react"
 function Navbar() {
   return (
     <>
@@ -9,16 +13,20 @@ function Navbar() {
         >
           <ul className="flex gap-12 justify-center items-center h-full dark:text-neutral-400 text-neutral-900 transition-all">
             <li className="hover:scale-110 hover:text-lime-500 dark:hover:text-lime-500 duration-300 capitalize">
-              <Link to={'/'} >home</Link>
+              <Link to={'/'} className="hidden md:inline" >home</Link>
+              <Link to={'/'} className="md:hidden"><Home /></Link>
             </li>
             <li className="hover:scale-110 hover:text-lime-500 dark:hover:text-lime-500 duration-300 capitalize">
-              <a href="#about">about</a>{" "}
+              <a href="#about" className="hidden md:inline">about</a>
+              <a href="#about" className="md:hidden"><User /></a>
             </li>
             <li className="hover:scale-110 hover:text-lime-500 dark:hover:text-lime-500 duration-300 capitalize">
-              <a href="#work">wok</a>
+              <a href="#work" className="hidden md:inline">wok</a>
+              <a href="#work" className="md:hidden"><BriefcaseBusiness /></a>
             </li>
             <li className="hover:scale-110 hover:text-lime-500 dark:hover:text-lime-500 duration-300 capitalize">
-              <a href="#contact">contact</a>
+              <a href="#contact" className="hidden md:inline">contact</a>
+              <a href="#contact" className="md:hidden"><Phone /></a>
             </li>
           </ul>
         </div>
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -11,18 +11,18 @@ function Timeline() {
     }, []);
     return (
     <div className="w-full h-fit mb-36 px-10 lg:px-28 ">
-        <h1 className="uppercase text-[4em] lg:text-[7em] font-bold text-neutral-200 dark:text-white/10 text-center" data-aos="fade-up" data-aos-duration="400">
+        <h1 className="uppercase text-[3em] lg:text-[7em] font-bold text-neutral-200 dark:text-white/10 text-center pt-28 lg:pt-0" data-aos="fade-up" data-aos-duration="400">
             Experience
         </h1>
       <div className="w-full rounded-lg overflow-hidden  flex justify-center flex-col items-center">
         {works.map((work, index) => (
-          <section className="border-b-[.5px] border-neutral-600 py-10 flex w-3/4 gap-10 justify-between"data-aos="fade-up" data-aos-duration="700">
-            <div className="w-fit">
+          <section className="border-b-[.5px] border-neutral-600 py-10 lg:flex-row flex-col flex lg:w-3/4 gap-10 justify-between"data-aos="fade-up" data-aos-duration="700">
+            <div className="w-full">
               <h3 className=" font-semibold text-neutral-600 dark:text-neutral-200 text-xl">{work.title}</h3>
               <span className="font-semibold text-lime-500 dark:text-lime-400 text-sm block uppercase">{work.place}</span>
               <span className="font-medium text-lime-700 dark:text-lime-200 text-sm">{work.date}</span>
             </div>
-            <div className="w-2/3">
+            <div className=" w-full lg:w-2/3">
                 <p className="text-neutral-800 dark:text-white font-medium">{work.description}</p>
             </div>
           </section>
